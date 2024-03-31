249ea55468b60198f48fe5ec031880eb7f5835aa
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-31 13:09:07 -0500

    Part 2 - Objective 7

--- R/week10.R
+++ R/week10.R
@@ -2,7 +2,9 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(haven)
-install
+install.packages("caret")
+library(caret)
+
 #### Data Import and Cleaning #### 
 # Import SPSS data (Road haven package)
 gss_original_tbl <- read_sav(file = "../data/GSS2016.sav")
@@ -10,7 +12,7 @@ gss_original_tbl <- read_sav(file = "../data/GSS2016.sav")
 # Create a variable gss_tbl
 gss_tbl <- gss_original_tbl %>%
   # Convert to data frame to ensure compatibility with dplyr functions by using as.data.frame
-  as.data.frame() %>%  
+  as_tibble() %>%  
   # Remove rows where MOSTHRS is missing by using filter and !is.na
   filter(!is.na(MOSTHRS)) %>%
   # Rename MOSTHRS to workhours by using rename
@@ -18,13 +20,32 @@ gss_tbl <- gss_original_tbl %>%
   # Remove HRS1 and HRS2 variables by using select 
   select(-c(HRS1, HRS2)) %>%
   # Retain only variables with less than 75% missingness by using select
-  select(where(~mean(is.na(.)) < 0.75))
+  select(where(~mean(is.na(.)) < 0.75)) 
 
 #### Visualization #### 
+# Making the histogram
 ggplot(gss_tbl, aes(x = workhours)) +
   geom_histogram() +
   labs(x = "Working hours",
        y = "Number of data",
        title = "Histogram of working hours")
 
+#### Analysis #### 
+## Prepare data set to run ML
+# Create sample rows  
+gss_sample <- sample(nrow(gss_tbl))
+# Shuffle using the sampled indices 
+gss_shuffle <- gss_tbl[gss_sample,]
+# Calculate the number of rows for a 75/25 split
+gss_75per <- round(nrow(gss_shuffle) * .75)
+# Create the training set using the first 75% of the shuffled data
+gss_train <- gss_shuffle[1:gss_75per,]
+# Create the test set using the remaining 25% of the shuffled data
+gss_test <- gss_shuffle[(gss_75per + 1):nrow(gss_shuffle),]
+# Create 10 folds for cross-validation using the workhours column from the training set
+gss_folds <- createFolds(gss_train$workhours, 10)
+# Set up train control
+gss_Control <- trainControl(method = "cv", number = 10, search = "grid", indexOut = gss_folds, verboseIter = TRUE)
+
+?trainControl
 
